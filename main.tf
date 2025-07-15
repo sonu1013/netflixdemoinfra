099a72def4327db81320dcc8a4bc6b0e73876210
sonu1013 <109957687+sonu1013@users.noreply.github.com>
2025-07-15 10:53:03 +0530

    Update main.tf
    updated resource

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
-region = "us-east-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-count = 4
-ami = "ami-04aa00acb1165b32a"
+count = 5
+ami = "ami-0d03cb826412c6b0f"
 instance_type = "t2.medium"
-key_name = "argocd"
-vpc_security_group_ids = ["sg-06785bc60f40dceda"]
+key_name = "sonu"
+vpc_security_group_ids = ["sg-048223c992445516c"]
 tags = {
 Name = var.instance_names[count.index]
 }
 }
 
 variable "instance_names" {
-default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+default = ["jenkins", "nexus", "tomcat-1", "tomcat-2", "Monitoring server"]
 }
