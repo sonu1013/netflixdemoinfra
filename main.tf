eb92ead290a1b1ebab71e115e57f4157519406fd
sonu1013 <109957687+sonu1013@users.noreply.github.com>
2025-07-15 10:58:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "one" {
 count = 5
 ami = "ami-0d03cb826412c6b0f"
 instance_type = "t2.medium"
-key_name = "sonu"
+key_name = "devops2"
 vpc_security_group_ids = ["sg-048223c992445516c"]
 tags = {
 Name = var.instance_names[count.index]
